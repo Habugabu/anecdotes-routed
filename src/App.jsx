3cae819ef60dca5abede5c17dc509e9355e46511
Habugabu <115887891+Habugabu@users.noreply.github.com>
2025-01-06 19:55:47 +0900

    7.6 completed

--- src/App.jsx
+++ src/App.jsx
@@ -82,9 +82,9 @@ const Notification = ({text}) => {
 
 const CreateNew = (props) => {
 
-  const content = useField('text')
-  const author = useField('text')
-  const info = useField('text')
+  const {reset: resetContent, ...content} = useField('text')
+  const {reset: resetAuthor, ...author} = useField('text')
+  const {reset: resetInfo, ...info} = useField('text')
 
   const navigate = useNavigate()
 
@@ -102,9 +102,9 @@ const CreateNew = (props) => {
   }
 
   const reset = () => {
-    content.reset()
-    author.reset()
-    info.reset()
+    resetContent()
+    resetAuthor()
+    resetInfo()
   }
 
   return (
